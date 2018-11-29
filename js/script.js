202d916b90b4bec463dbf1796ddea6f1fd6ed24e
raescano <raescano@hawaii.edu>
2018-11-29 08:54:41 -1000

    added javascript

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var mymenubutton = document.querySelector(".menu-button");
+
+    console.log(mymenubutton);
+
+    mymenubutton.onClick = function () {
+        console.log("yo whatup!");
+    };
+
+
+
+
+
+
+
+
+
+
+
+});
